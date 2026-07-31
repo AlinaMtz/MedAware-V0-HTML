92e846a9dabea45a6df05cbb02e51a937ba7f3ff
AlinaMtz <alinamartinez707@gmail.com>
2026-07-31 14:26:45 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,45 @@
+const retos = {
+
+ACV:[
+
+{
+
+pregunta:"¿Cuál es una señal de un ACV?",
+
+opciones:[
+"Debilidad en un lado",
+"Dolor muscular",
+"Picazón",
+"Mareo al levantarse"
+],
+
+correcta:0,
+
+explicacion:"La debilidad repentina puede indicar un ACV."
+
+},
+
+],
+
+Infarto:[
+
+{
+
+pregunta:"¿Qué síntoma es típico?",
+
+opciones:[
+"Dolor en el pecho",
+"Picazón",
+"Fiebre",
+"Hipo"
+],
+
+correcta:0,
+
+explicacion:"El dolor en el pecho es un síntoma frecuente."
+
+}
+
+]
+
+};
